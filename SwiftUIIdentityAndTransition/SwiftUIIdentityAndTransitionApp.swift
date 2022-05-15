8943d589900b624f72c05c9fa221589601c56724
Raheel Ahmad <ahmadraheel@gmail.com>
2022-05-14 17:48:39 -0700

    Initial Commit

---
+++ SwiftUIIdentityAndTransition/SwiftUIIdentityAndTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIdentityAndTransitionApp.swift
+//  SwiftUIIdentityAndTransition
+//
+//  Created by Raheel Ahmad on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIdentityAndTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
